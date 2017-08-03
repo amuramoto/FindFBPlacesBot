48aacba063a72562dc81a5c8ddaa4f9c7bfa6179
Alex Muramoto <amuramoto@apache.org>
2017-08-03 16:21:44 -0700

    debugging

--- index.js
+++ index.js
@@ -147,25 +147,30 @@ function sendMessage (ps_user_id, type, message_payload) {
 }
 
 function postSenderAction (sender_action, ps_user_id, callback) {
-console.log(sender_action);	
+	let timeout = 0;
 	let request_body = {
 		recipient: {
 			id: ps_user_id 			
 		},
 		sender_action: sender_action
 	}
-console.log(request_body.sender_action);
+
+	if (sender_action = 'mark_seen') {
+		timeout = 2000;
+	}
 	setTimeout(() => {
 		request.post(messenger_api_url, {form: request_body}, (err, res, body) => {
 			if (callback) {
-				callback();
+				setTimeout(() => {
+					callback();
+				}, 4000);				
 			}
 
 			if (err) {
 				console.error(err);
 			}
 		})
-	}, 1000);
+	}, timeout);
 }
 
 function getUserInfo (ps_user_id, callback) {
