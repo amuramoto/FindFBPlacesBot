95f4cbb9db30fbe32c21527be2dbe6da688fa4a8
Alex Muramoto <amuramoto@apache.org>
2017-08-03 14:13:34 -0700

    debugging

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook', (req, res) => {
       // Iterate over each messaging event
       pageEntry.messaging.forEach(messagingEvent => {
         if (messagingEvent.message) {
-          console.log(JSON.stringify(messagingEvent.message));
+          console.log(JSON.stringify(messagingEvent));
           handleMessage(messagingEvent);
         } else if (messagingEvent.delivery) {
           // handleDeliveryConfirmation(messagingEvent);
